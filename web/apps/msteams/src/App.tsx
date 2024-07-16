8ff9786643581c9c78c2aca0a8c8d4fdfd6c4aca
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2024-07-17 09:39:05 +1200

    Add react router to msteams web app

--- web/apps/msteams/src/App.tsx
+++ web/apps/msteams/src/App.tsx
@@ -1,20 +1,15 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import logo from './logo.svg';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
