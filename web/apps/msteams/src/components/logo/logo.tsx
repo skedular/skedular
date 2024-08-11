63c95e0228927c741eb013389df62822676de2ed
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2024-08-11 18:12:24 +1200

    Add logo to msteams app

--- web/apps/msteams/src/components/logo/logo.tsx
+++ web/apps/msteams/src/components/logo/logo.tsx
@@ -1,14 +1,14 @@
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import Image from 'next/image';
+import Link from 'next/link';
 import { memo } from 'react';
 
 const Logo = () => {
   return (
     <Link href="/" style={{ textDecoration: 'none' }}>
       <Stack direction="row">
-        <Box component="img" src="/images/logo.png" sx={{ width: 60, height: 60 }} alt="UnityHub Logo" />
+        <Image src="/images/logo.png" width={60} height={60} alt="UnityHub Logo" />
         <Typography
           variant="h4"
           sx={{
