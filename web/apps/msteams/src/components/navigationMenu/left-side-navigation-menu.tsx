21bc9d9d747afd66f62cac09d55a09143dfd8547
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2024-08-11 17:58:32 +1200

    Fix left side nagivation links in msteams app

--- web/apps/msteams/src/components/navigationMenu/left-side-navigation-menu.tsx
+++ web/apps/msteams/src/components/navigationMenu/left-side-navigation-menu.tsx
@@ -1,11 +1,11 @@
-import { CalendarIcon, LocationIcon, NotificationsIcon, OrganizationIcon, SettingsIcon, TeamIcon } from '@repo/shared/components/icons';
 import Divider from '@mui/material/Divider';
+import Link from '@mui/material/Link';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import Link from 'next/link';
+import { CalendarIcon, LocationIcon, NotificationsIcon, OrganizationIcon, SettingsIcon, TeamIcon } from '@repo/shared/components/icons';
 import { memo } from 'react';
 
 const LeftSideNavigationMenu = () => {
