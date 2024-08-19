178be3d32eb21cd41bb27d922af6147f147aa675
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2024-08-19 19:31:47 +1200

    Removed msteams resource from msteams domain

--- msteams/shared/infrastructure/workspaces/common/outputs.tf
+++ msteams/shared/infrastructure/workspaces/common/outputs.tf
@@ -4,27 +4,3 @@ output "tags" {
     domain = "msteams"
   }
 }
-
-output "parameter_store_name_azure_msteams_application_id" {
-  value = "azure_msteams_application_id"
-}
-
-output "parameter_store_name_azure_msteams_secret_id" {
-  value = "azure_msteams_secret_id"
-}
-
-output "parameter_store_name_azure_msteams_secret_value" {
-  value = "azure_msteams_secret_value"
-}
-
-output "parameter_store_name_azure_msteams_dev_application_id" {
-  value = "azure_msteams_dev_application_id"
-}
-
-output "parameter_store_name_azure_msteams_dev_secret_id" {
-  value = "azure_msteams_dev_secret_id"
-}
-
-output "parameter_store_name_azure_msteams_dev_secret_value" {
-  value = "azure_msteams_dev_secret_value"
-}
