786cc71cb6d7548c7b0f79d4d5e9ab92b6808919
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2024-08-24 15:12:18 +1200

    Fixed Azure AD related permissions

--- web/packages/shared/src/libs/nextauth/index.ts
+++ web/packages/shared/src/libs/nextauth/index.ts
@@ -165,7 +165,7 @@ const authOptions: AuthOptions = {
       clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
       authorization: {
         params: {
-          scope: 'email offline_access openid profile',
+          scope: 'ProfilePhoto.Read.All Contacts.Read email offline_access openid profile',
         },
       },
     }),
