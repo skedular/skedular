884c2b78f80e7185d28478a25e8635d8b2132870
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2024-08-25 15:15:56 +1200

    Fixed webapps

--- web/apps/msteams/infrastructure/workspaces/staging/backend_webapp_msteams.tf
+++ web/apps/msteams/infrastructure/workspaces/staging/backend_webapp_msteams.tf
@@ -7,7 +7,6 @@ terraform {
     key            = "terraform.tfstate"
     dynamodb_table = "unityhub-staging-webapp-msteams-terraform-state-lock"
     profile        = ""
-    role_arn       = ""
     encrypt        = "true"
   }
 }
