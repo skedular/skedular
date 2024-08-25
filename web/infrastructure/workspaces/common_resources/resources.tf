8100ee2402ff3578976c4e0608311030d3b35d12
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2024-08-25 15:00:55 +1200

    Remove all web apps resources

--- web/infrastructure/workspaces/common_resources/resources.tf
+++ web/infrastructure/workspaces/common_resources/resources.tf
@@ -4,15 +4,15 @@ module "common" {
   environment = var.environment
 }
 
-resource "random_password" "nextauthsecret" {
-  length           = 256
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
-}
+# resource "random_password" "nextauthsecret" {
+#   length           = 256
+#   special          = true
+#   override_special = "!#$%&*()-_=+[]{}<>:?"
+# }
 
-resource "aws_ssm_parameter" "nextauthsecret" {
-  name  = module.common.parameter_store_name_nextauth_session
-  type  = "String"
-  value = random_password.nextauthsecret.result
-  tags  = local.tags
-}
+# resource "aws_ssm_parameter" "nextauthsecret" {
+#   name  = module.common.parameter_store_name_nextauth_session
+#   type  = "String"
+#   value = random_password.nextauthsecret.result
+#   tags  = local.tags
+# }
