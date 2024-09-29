69814ce737d2ae7095ade071d6339cf0984f20ba
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2024-09-29 14:31:54 +1300

    Fixed broken build

--- web/packages/shared/src/components/link/link.tsx
+++ web/packages/shared/src/components/link/link.tsx
@@ -5,13 +5,17 @@ import type { UrlObject } from 'url';
 
 type Url = string | UrlObject;
 
+type HTMLAttributeAnchorTarget = '_self' | '_blank' | '_parent' | '_top' | (string & {});
+
 type Props = {
   children: React.ReactNode;
   href: Url;
+  target?: HTMLAttributeAnchorTarget | undefined;
+  rel?: string | undefined;
 };
 
-const Link = ({ children, href }: Props) => (
-  <NextLink href={href}>
+const Link = ({ children, href, target, rel }: Props) => (
+  <NextLink href={href} target={target} rel={rel}>
     <MUILink>{children}</MUILink>
   </NextLink>
 );
