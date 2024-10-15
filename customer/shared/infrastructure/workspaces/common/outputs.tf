2cf119baf8b390102455e94d1877345f6e9501fd
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2024-10-15 21:38:14 +1300

    Add new customer joined through web email template

--- customer/shared/infrastructure/workspaces/common/outputs.tf
+++ customer/shared/infrastructure/workspaces/common/outputs.tf
@@ -12,3 +12,11 @@ output "new_customer_feedback_email_template_name" {
 output "parameter_store_name_new_customer_feedback_email_template_arn" {
   value = "new_customer_feedback_through_web_template_arn"
 }
+
+output "new_customer_joined_email_template_name" {
+  value = "NewCustomerJoinedThroughWeb"
+}
+
+output "parameter_store_name_new_customer_joined_email_template_arn" {
+  value = "new_customer_joined_through_web_template_arn"
+}
