ea6a08f284717e48d4c9bbe7dc0d5f4a809c30f3
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2024-10-19 18:14:03 +1300

    Add appbar component

---
+++ web/apps/msteams/src/components/appBar/__generated__/appBar_query.graphql.ts
@@ -0,0 +1,114 @@
+/**
+ * @generated SignedSource<<cd7729ada8e29852112425de6d60218e>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ReaderFragment } from 'relay-runtime';
+import { FragmentRefs } from "relay-runtime";
+export type appBar_query$data = {
+  readonly me: {
+    readonly email: {
+      readonly email: string;
+      readonly verified: boolean;
+    } | null | undefined;
+    readonly familyName: string | null | undefined;
+    readonly givenName: string | null | undefined;
+    readonly middleName: string | null | undefined;
+    readonly photoUrl: string | null | undefined;
+  } | null | undefined;
+  readonly " $fragmentSpreads": FragmentRefs<"newFeedbackDialog_query">;
+  readonly " $fragmentType": "appBar_query";
+};
+export type appBar_query$key = {
+  readonly " $data"?: appBar_query$data;
+  readonly " $fragmentSpreads": FragmentRefs<"appBar_query">;
+};
+
+const node: ReaderFragment = {
+  "argumentDefinitions": [],
+  "kind": "Fragment",
+  "metadata": null,
+  "name": "appBar_query",
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "concreteType": "CustomerDetails",
+      "kind": "LinkedField",
+      "name": "me",
+      "plural": false,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "CustomerEmail",
+          "kind": "LinkedField",
+          "name": "email",
+          "plural": false,
+          "selections": [
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "email",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "verified",
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "givenName",
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "middleName",
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "familyName",
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "photoUrl",
+          "storageKey": null
+        }
+      ],
+      "storageKey": null
+    },
+    {
+      "args": null,
+      "kind": "FragmentSpread",
+      "name": "newFeedbackDialog_query"
+    }
+  ],
+  "type": "Query",
+  "abstractKey": null
+};
+
+(node as any).hash = "f9126c8fb1b1594c6de54f92d139f9ed";
+
+export default node;
---
+++ web/apps/msteams/src/components/appBar/app-bar.tsx
@@ -0,0 +1,179 @@
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Stack from '@mui/material/Stack';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+import { CustomerAvatar } from '@repo/shared/components/avatars';
+import { FeedbackIcon, SettingsIcon } from '@repo/shared/components/icons';
+import { PaletteModeContext, UpdatePaletteModeContext } from '@repo/shared/libs/providers';
+import { getCustomerFullName } from '@repo/shared/libs/utils';
+import graphql from 'babel-plugin-relay/macro';
+import { NewFeedbackDialog } from 'components/feedback';
+import { memo, useContext, useState } from 'react';
+import { useFragment } from 'react-relay';
+import type { appBar_query$key } from './__generated__/appBar_query.graphql';
+
+type Props = {
+  rootDataRelay: appBar_query$key;
+  breadcrumb?: AppBarBreadcrumb;
+};
+
+type AppBarBreadcrumbItem = {
+  label: string;
+  href: string;
+};
+
+export type AppBarBreadcrumb = {
+  items?: AppBarBreadcrumbItem[];
+  lastItemLabel?: string;
+};
+
+const AppBar = ({ rootDataRelay, breadcrumb }: Props) => {
+  const rootData = useFragment<appBar_query$key>(
+    graphql`
+      fragment appBar_query on Query {
+        me {
+          email {
+            email
+            verified
+          }
+          givenName
+          middleName
+          familyName
+          photoUrl
+        }
+        ...newFeedbackDialog_query
+      }
+    `,
+    rootDataRelay,
+  );
+
+  const paletteMode = useContext(PaletteModeContext);
+  const updatePaletteMode = useContext(UpdatePaletteModeContext);
+  const [profileOpenAnchorEl, setProfileOpenAnchorEl] = useState<null | HTMLElement>(null);
+  const [submitFeedbackDialogOpen, setSubmitFeedbackDialogOpen] = useState(false);
+
+  const handleProfileMenuOpenClick = (event: React.MouseEvent<HTMLElement>) => {
+    setProfileOpenAnchorEl(event.currentTarget);
+  };
+
+  const handleProfileMenuCloseClick = () => {
+    setProfileOpenAnchorEl(null);
+  };
+
+  const handleSubmitFeedbackSendClick = () => {
+    setSubmitFeedbackDialogOpen(false);
+  };
+
+  const handleSubmitFeedbackCancelClick = () => {
+    setSubmitFeedbackDialogOpen(false);
+  };
+
+  return (
+    <>
+      <Stack direction="row" sx={{ alignItems: 'center', justifyContent: 'space-between', width: '100%', paddingLeft: 1, paddingRight: 1 }}>
+        <Breadcrumbs maxItems={3}>
+          {breadcrumb?.items?.map((item, index) => (
+            <Link key={index} underline="hover" href={item.href}>
+              <Typography>{item.label}</Typography>
+            </Link>
+          ))}
+          {breadcrumb?.lastItemLabel && <Typography>{breadcrumb?.lastItemLabel}</Typography>}
+        </Breadcrumbs>
+
+        <Stack direction="row" spacing={1} sx={{ alignItems: 'center' }}>
+          <Tooltip title="Send us feedback">
+            <IconButton sx={{ ml: 1 }} onClick={() => setSubmitFeedbackDialogOpen(true)}>
+              <FeedbackIcon />
+            </IconButton>
+          </Tooltip>
+
+          {paletteMode === 'dark' && (
+            <IconButton sx={{ ml: 1 }} onClick={() => updatePaletteMode('light')}>
+              <LightModeIcon />
+            </IconButton>
+          )}
+
+          {paletteMode === 'light' && (
+            <IconButton sx={{ ml: 1 }} onClick={() => updatePaletteMode('dark')}>
+              <DarkModeIcon />
+            </IconButton>
+          )}
+
+          <IconButton onClick={handleProfileMenuOpenClick}>
+            <CustomerAvatar
+              name={{
+                name: null,
+                givenName: rootData.me?.givenName,
+                middleName: rootData.me?.middleName,
+                familyName: rootData.me?.familyName,
+              }}
+              photo={{
+                url: rootData.me?.photoUrl,
+              }}
+            />
+          </IconButton>
+
+          <Menu
+            sx={{ mt: 4 }}
+            anchorEl={profileOpenAnchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(profileOpenAnchorEl)}
+            onClose={handleProfileMenuCloseClick}
+          >
+            <MenuItem>
+              <Stack direction="column">
+                <Stack direction="column">
+                  <Typography variant="body1">Signed in as</Typography>
+                  <Typography variant="body1">
+                    {getCustomerFullName({
+                      name: null,
+                      givenName: rootData.me?.givenName,
+                      middleName: rootData.me?.middleName,
+                      familyName: rootData.me?.familyName,
+                    })}
+                  </Typography>
+                  {rootData.me?.email && <Typography variant="body1">{rootData.me?.email.email}</Typography>}
+                </Stack>
+              </Stack>
+            </MenuItem>
+
+            <Divider />
+
+            <MenuItem>
+              <Link href="/settings" color="inherit">
+                <Stack direction="row" spacing={1} sx={{ alignItems: 'center' }}>
+                  <SettingsIcon fontSize="small" />
+                  <Typography textAlign="center">Settings</Typography>
+                </Stack>
+              </Link>
+            </MenuItem>
+          </Menu>
+        </Stack>
+      </Stack>
+
+      <NewFeedbackDialog
+        rootDataRelay={rootData}
+        isDialogOpen={submitFeedbackDialogOpen}
+        onSendClicked={handleSubmitFeedbackSendClick}
+        onCancelClicked={handleSubmitFeedbackCancelClick}
+      />
+    </>
+  );
+};
+
+export default memo(AppBar);
