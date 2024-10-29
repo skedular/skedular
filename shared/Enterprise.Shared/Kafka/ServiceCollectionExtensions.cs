e20274f554e59f048bfcc313992dc7455cec535d
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2024-10-29 16:29:20 +1300

    Disable telemtry

--- shared/Enterprise.Shared/Kafka/ServiceCollectionExtensions.cs
+++ shared/Enterprise.Shared/Kafka/ServiceCollectionExtensions.cs
@@ -21,7 +21,7 @@ public static class ServiceCollectionExtensions
 
     public static IServiceCollection AddKafka(
         this IServiceCollection services,
-        bool useTelemetry = true)
+        bool useTelemetry = false)
     {
         services
             .AddSingleton(typeof(IKafkaPublisher<,>), typeof(KafkaPublisher<,>))
