4c482168f3ce6c295ac39732efeef92e08307c95
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2024-11-07 22:08:20 +1300

    Add experimental react compiler

--- web/apps/webapp/next.config.js
+++ web/apps/webapp/next.config.js
@@ -3,6 +3,9 @@ const relay = require("./relay.config");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
   compiler: {
     relay,
   },
