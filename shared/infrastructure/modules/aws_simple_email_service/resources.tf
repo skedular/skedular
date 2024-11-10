dbd69a20892d6031a5da78ee99a43e2530c9b68d
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2024-11-11 07:36:44 +1300

    Setting dmarc values

--- shared/infrastructure/modules/aws_simple_email_service/resources.tf
+++ shared/infrastructure/modules/aws_simple_email_service/resources.tf
@@ -10,12 +10,21 @@ data "cloudflare_zone" "default" {
   name = var.cloudflare_domain
 }
 
-resource "cloudflare_record" "default" {
+resource "cloudflare_record" "dkim" {
   count   = 3
   zone_id = data.cloudflare_zone.default.id
   name    = "${aws_ses_domain_dkim.default.dkim_tokens[count.index]}._domainkey.${aws_ses_domain_identity.default.domain}"
   content = "${aws_ses_domain_dkim.default.dkim_tokens[count.index]}.dkim.amazonses.com"
   type    = "CNAME"
+  proxied = false
+  ttl     = 600
+}
+
+resource "cloudflare_record" "dmarc" {
+  zone_id = data.cloudflare_zone.default.id
+  name    = "_dmarc.${aws_ses_domain_identity.default.domain}"
+  content = "\"v=DMARC1; p=none;\""
+  type    = "TXT"
   proxied = false
   ttl     = 600
 }
