a146e33b374afc98f856d5c35f078fef85f6f639
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2024-11-12 00:33:48 +1300

    Moved Event Catalog to domain skedular.app

--- web/event-catalog/infrastructure/workspaces/common_resources/resources.tf
+++ web/event-catalog/infrastructure/workspaces/common_resources/resources.tf
@@ -35,7 +35,7 @@ resource "vercel_project" "default" {
 resource "vercel_project_domain" "default" {
   project_id = vercel_project.default.id
   team_id    = local.team_id
-  domain     = module.shared_common.eventcatalog_webapp_domain_name_1
+  domain     = module.shared_common.eventcatalog_webapp_domain_name_2
 }
 
 data "vercel_project_directory" "default" {
@@ -50,13 +50,13 @@ resource "vercel_deployment" "default" {
   team_id     = local.team_id
 }
 
-data "cloudflare_zone" "default_1" {
-  name = module.shared_common.cloudflare_webapp_domain_name_1
+data "cloudflare_zone" "default" {
+  name = module.shared_common.cloudflare_webapp_domain_name_2
 }
 
 resource "cloudflare_record" "default" {
-  zone_id = data.cloudflare_zone.default_1.id
-  name    = module.shared_common.eventcatalog_webapp_domain_name_1
+  zone_id = data.cloudflare_zone.default.id
+  name    = module.shared_common.eventcatalog_webapp_domain_name_2
   content = "cname.vercel-dns.com."
   type    = "CNAME"
   proxied = false
