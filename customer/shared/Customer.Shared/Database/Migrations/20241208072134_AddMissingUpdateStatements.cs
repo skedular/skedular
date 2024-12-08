c5290f2ee29696a85130f8871c8c5e31907a9680
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2024-12-08 20:26:58 +1300

    Added missing update statements

---
+++ customer/shared/Customer.Shared/Database/Migrations/20241208072134_AddMissingUpdateStatements.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Customer.Shared.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingUpdateStatements : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE public.""LocationMember""
+                                    SET ""NewMembershipType""= CASE 
+                                        WHEN ""MembershipType"" = 0 THEN 'OWNER'
+                                        WHEN ""MembershipType"" = 1 THEN 'ADMINISTRATOR'
+                                        WHEN ""MembershipType"" = 2 THEN 'MEMBER'
+                                        ELSE 'UNKNOWN'
+                                END");
+            
+            migrationBuilder.Sql(@"UPDATE public.""TeamMember""
+                                    SET ""NewMembershipType""= CASE 
+                                        WHEN ""MembershipType"" = 0 THEN 'OWNER'
+                                        WHEN ""MembershipType"" = 1 THEN 'ADMINISTRATOR'
+                                        WHEN ""MembershipType"" = 2 THEN 'MEMBER'
+                                        ELSE 'UNKNOWN'
+                                END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ team/shared/Team.Shared/Database/Migrations/20241208072545_AddMissingUpdateStatements.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Team.Shared.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingUpdateStatements : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE public.""TeamMember""
+                                    SET ""NewMembershipType""= CASE 
+                                        WHEN ""MembershipType"" = 0 THEN 'OWNER'
+                                        WHEN ""MembershipType"" = 1 THEN 'ADMINISTRATOR'
+                                        WHEN ""MembershipType"" = 2 THEN 'MEMBER'
+                                        ELSE 'UNKNOWN'
+                                END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
