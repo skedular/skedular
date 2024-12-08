675bca74ff2e4711a9da7f7d6268152a9a794220
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2024-12-08 19:50:55 +1300

    Set Membership Type and Invitation Status columns using the old integer values

---
+++ slack/shared/Slack.Shared/Database/Migrations/20241208063703_SetMembershipTypeColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Slack.Shared.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetMembershipTypeColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE public.""OrganizationMember""
+                                    SET ""NewMembershipType""= CASE 
+                                        WHEN ""MembershipType"" = 0 THEN 'OWNER'
+                                        WHEN ""MembershipType"" = 1 THEN 'ADMINISTRATOR'
+                                        WHEN ""MembershipType"" = 2 THEN 'MEMBER'
+                                        ELSE 'UNKNOWN'
+                                END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ team/shared/Team.Shared/Database/Migrations/20241208063702_SetMembershipTypeColumn.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Team.Shared.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetMembershipTypeColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE public.""OrganizationMember""
+                                    SET ""NewMembershipType""= CASE 
+                                        WHEN ""MembershipType"" = 0 THEN 'OWNER'
+                                        WHEN ""MembershipType"" = 1 THEN 'ADMINISTRATOR'
+                                        WHEN ""MembershipType"" = 2 THEN 'MEMBER'
+                                        ELSE 'UNKNOWN'
+                                END");
+            
+            migrationBuilder.Sql(@"UPDATE public.""JoinInvitation""
+                                    SET ""NewStatus""= CASE 
+                                        WHEN ""MembershipType"" = 0 THEN 'PENDING'
+                                        WHEN ""MembershipType"" = 1 THEN 'ACCEPTED'
+                                        WHEN ""MembershipType"" = 2 THEN 'REJECTED'
+                                        WHEN ""MembershipType"" = 3 THEN 'CANCELLED'
+                                        ELSE 'UNKNOWN'
+                                END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
