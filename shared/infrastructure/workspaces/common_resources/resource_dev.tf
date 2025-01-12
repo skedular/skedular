7e9424c5b132c85df9091f2586476641a97d2909
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2025-01-12 19:54:28 +1300

    Add kowl and dozzle dns records

--- shared/infrastructure/workspaces/common_resources/resource_dev.tf
+++ shared/infrastructure/workspaces/common_resources/resource_dev.tf
@@ -1,7 +1,7 @@
 locals {
   azure_app_display_name_dev = "UnityHub-dev"
   azure_app_description_dev  = "UnityHub-dev"
-  dns_records_dev            = ["aweb", "aapp", "amsteams", "dweb", "dapp", "dmsteams", "mweb", "mapp", "mmsteams", "yweb", "yapp", "ymsteams", "contabo"]
+  dns_records_dev            = ["aweb", "aapp", "amsteams", "dweb", "dapp", "dmsteams", "mweb", "mapp", "mmsteams", "yweb", "yapp", "ymsteams", "contabo", "kowl", "dozzle"]
 }
 
 resource "cloudflare_record" "cloudflare_dns_records_dev" {
