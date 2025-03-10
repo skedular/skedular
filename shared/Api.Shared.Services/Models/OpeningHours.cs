76b7ee7919328a66d4be1f04cd09dc30de8d162f
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2025-03-10 18:33:14 +1300

    Add opening hourse

---
+++ shared/Api.Shared.Services/Models/OpeningHours.cs
@@ -0,0 +1,36 @@
+﻿namespace Api.Shared.Services.Models;
+
+public class OpeningHour
+{
+    public OpeningHour(TimeOnly from, TimeOnly until)
+    {
+        if (from >= until)
+        {
+            throw new ArgumentOutOfRangeException(nameof(from), from, "from must be less than until");
+        }
+
+        if (!IsMultipleOf15(from) || !IsMultipleOf15(until))
+        {
+            throw new ArgumentException("Both from and until must be in 15-minute increments.");
+        }
+
+        From = from;
+        Until = until;
+    }
+
+    public TimeOnly From { get; }
+    public TimeOnly Until { get; }
+
+    private static bool IsMultipleOf15(TimeOnly time) => time.Minute % 15 == 0;
+}
+
+public class OpeningHours(
+    OpeningHour Monday,
+    OpeningHour Tuesday,
+    OpeningHour Wednesday,
+    OpeningHour Thursday,
+    OpeningHour Friday,
+    OpeningHour Saturday,
+    OpeningHour Sunday,
+    ICollection<DateTimeOffset> ClosedDates,
+    Dictionary<DateTimeOffset, OpeningHours> DatesWithVariedOpeningHours);
