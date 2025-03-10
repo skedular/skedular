9e950c22090cca21567ff97a63378ec34e8a1844
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2025-03-11 08:55:16 +1300

    Temporary removed msteams support

--- web/apps/webapp/src/libs/providers/in-msteams-provider.tsx
+++ web/apps/webapp/src/libs/providers/in-msteams-provider.tsx
@@ -3,8 +3,12 @@ import { createContext } from 'react';
 
 export const InMsTeamsContext = createContext<boolean>(typeof window !== 'undefined' && window.name === 'embedded-page-container');
 
-const InMsTeamsProvider = ({ children }: PropsWithChildren) => {
-  return <InMsTeamsContext.Provider value={typeof window !== 'undefined' && window.name === 'embedded-page-container'}>{children}</InMsTeamsContext.Provider>;
-};
+// const InMsTeamsProvider = ({ children }: PropsWithChildren) => (
+//   <InMsTeamsContext.Provider value={typeof window !== 'undefined' && window.name === 'embedded-page-container'}>{children}</InMsTeamsContext.Provider>
+// );
+
+const InMsTeamsProvider = ({ children }: PropsWithChildren) => (
+  <InMsTeamsContext.Provider value={false}>{children}</InMsTeamsContext.Provider>
+);
 
 export default InMsTeamsProvider;
