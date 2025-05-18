ce7ba8d2593421709ba30d2bd8311c26d7754635
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2025-05-18 20:59:14 +1200

    GraphQL API refactoring

--- gateway/apis/Gateway/ServiceDiscoveryConfigurationRewriter.cs
+++ gateway/apis/Gateway/ServiceDiscoveryConfigurationRewriter.cs
@@ -8,17 +8,50 @@ public class ServiceDiscoveryConfigurationRewrite(SubgraphsConfigurations subgra
     protected override ValueTask<HttpClientConfiguration> RewriteAsync(HttpClientConfiguration configuration, CancellationToken cancellationToken) =>
         new(configuration.SubgraphName switch
         {
-            nameof(subgraphsConfigurations.Billing) => configuration with { EndpointUri = subgraphsConfigurations.Billing.Uri! },
-            nameof(subgraphsConfigurations.Booking) => configuration with { EndpointUri = subgraphsConfigurations.Booking.Uri! },
-            nameof(subgraphsConfigurations.Customer) => configuration with { EndpointUri = subgraphsConfigurations.Customer.Uri! },
-            nameof(subgraphsConfigurations.Location) => configuration with { EndpointUri = subgraphsConfigurations.Location.Uri! },
-            nameof(subgraphsConfigurations.Marketplace) => configuration with { EndpointUri = subgraphsConfigurations.Marketplace.Uri! },
-            nameof(subgraphsConfigurations.MsTeams) => configuration with { EndpointUri = subgraphsConfigurations.MsTeams.Uri! },
-            nameof(subgraphsConfigurations.Notification) => configuration with { EndpointUri = subgraphsConfigurations.Notification.Uri! },
-            nameof(subgraphsConfigurations.Organization) => configuration with { EndpointUri = subgraphsConfigurations.Organization.Uri! },
-            nameof(subgraphsConfigurations.Payment) => configuration with { EndpointUri = subgraphsConfigurations.Payment.Uri! },
-            nameof(subgraphsConfigurations.Slack) => configuration with { EndpointUri = subgraphsConfigurations.Slack.Uri! },
-            nameof(subgraphsConfigurations.Team) => configuration with { EndpointUri = subgraphsConfigurations.Team.Uri! },
+            nameof(subgraphsConfigurations.Billing) => configuration with
+            {
+                EndpointUri = subgraphsConfigurations.Billing.Uri ?? new Uri("https+http://billingapi/v1/graphql")
+            },
+            nameof(subgraphsConfigurations.Booking) => configuration with
+            {
+                EndpointUri = subgraphsConfigurations.Booking.Uri ?? new Uri("https+http://bookingapi/v1/graphql")
+            },
+            nameof(subgraphsConfigurations.Customer) => configuration with
+            {
+                EndpointUri = subgraphsConfigurations.Customer.Uri ?? new Uri("https+http://customerapi/v1/graphql")
+            },
+            nameof(subgraphsConfigurations.Location) => configuration with
+            {
+                EndpointUri = subgraphsConfigurations.Location.Uri ?? new Uri("https+http://locationapi/v1/graphql")
+            },
+            nameof(subgraphsConfigurations.Marketplace) => configuration with
+            {
+                EndpointUri = subgraphsConfigurations.Marketplace.Uri ?? new Uri("https+http://marketplaceapi/v1/graphql")
+            },
+            nameof(subgraphsConfigurations.MsTeams) => configuration with
+            {
+                EndpointUri = subgraphsConfigurations.MsTeams.Uri ?? new Uri("https+http://msteamsapi/v1/graphql")
+            },
+            nameof(subgraphsConfigurations.Notification) => configuration with
+            {
+                EndpointUri = subgraphsConfigurations.Notification.Uri ?? new Uri("https+http://notificationapi/v1/graphql")
+            },
+            nameof(subgraphsConfigurations.Organization) => configuration with
+            {
+                EndpointUri = subgraphsConfigurations.Organization.Uri ?? new Uri("https+http://organizationapi/v1/graphql")
+            },
+            nameof(subgraphsConfigurations.Payment) => configuration with
+            {
+                EndpointUri = subgraphsConfigurations.Payment.Uri ?? new Uri("https+http://paymentapi/v1/graphql")
+            },
+            nameof(subgraphsConfigurations.Slack) => configuration with
+            {
+                EndpointUri = subgraphsConfigurations.Slack.Uri ?? new Uri("https+http://slackapi/v1/graphql")
+            },
+            nameof(subgraphsConfigurations.Team) => configuration with
+            {
+                EndpointUri = subgraphsConfigurations.Team.Uri ?? new Uri("https+http://teamapi/v1/graphql")
+            },
             _ => throw new NotSupportedException()
         });
 }
