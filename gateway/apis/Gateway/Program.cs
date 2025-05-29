6b1a5c0e288f22913f07b29f43358b0a8b246e01
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2025-05-29 21:34:05 +1200

    Fix local development issue

--- gateway/apis/Gateway/Program.cs
+++ gateway/apis/Gateway/Program.cs
@@ -31,10 +31,20 @@ public class Program
             .AddSingleton<IConfigurationRewriter, ServiceDiscoveryConfigurationRewrite>()
             .AddScoped<ApiAuthenticationHttpClientHandler>();
 
+        var filename = Path.GetTempFileName();
+        using (var embeddedGatewayFileStream = typeof(Program).Assembly.GetManifestResourceStream($"{typeof(Program).Namespace}.gateway.fgp"))
+        {
+            ArgumentNullException.ThrowIfNull(embeddedGatewayFileStream);
+            using (var fileStream = File.OpenWrite(filename))
+            {
+                embeddedGatewayFileStream.CopyTo(fileStream);
+            }
+        }
+
         var fusionGatewayBuilder =
             services
                 .AddFusionGatewayServer()
-                .ConfigureFromFile("gateway.fgp");
+                .ConfigureFromFile(filename);
 
         if (environment.IsDevelopment())
         {
