333d61f3f3d9d43659c118d4b12f6bf280b6aee6
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2025-07-17 21:30:32 +1200

    Fix UI build issue

--- web/apps/webapp/src/components/organization/organizationAdmin/organization-admin.tsx
+++ web/apps/webapp/src/components/organization/organizationAdmin/organization-admin.tsx
@@ -1283,6 +1283,7 @@ const OrganizationAdmin = ({ rootDataRelay, rootDataOrganizationRelay, rootDataZ
           organization: {
             id: organization.id,
             taxDetails: {
+              id: '',
               taxId,
               taxRatePercentage,
             },
