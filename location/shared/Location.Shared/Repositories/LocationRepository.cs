1578a28a844ae878ae2005c9f9ffd09bc4d6b768
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2025-10-05 21:48:01 +1300

    Remove caching organization members for location

--- location/shared/Location.Shared/Repositories/LocationRepository.cs
+++ location/shared/Location.Shared/Repositories/LocationRepository.cs
@@ -44,7 +44,6 @@ internal static class LocationExtensions
         bool includeDeletedResources) =>
         (isTracked ? originalQuery.AsTracking() : originalQuery.AsNoTrackingWithIdentityResolution())
         .Include(query => query.Organization)
-        .ThenInclude(query => query.OrganizationMembers.Where(organizationMember => !organizationMember.DeletedAt.HasValue))
         .Include(query => query.PhysicalAddress)
         .Include(query => query.Resources.Where(resource => includeDeletedResources || !resource.DeletedAt.HasValue))
         .ThenInclude(query => query.OrganizationTags.Where(tag => !tag.DeletedAt.HasValue))
