c4e895c5f38aa9cb80b396d6d59fc8aa1f36f6ab
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2025-10-11 13:02:08 +1300

    Added into page to doc

--- web/apps/webapphelp/next.config.ts
+++ web/apps/webapphelp/next.config.ts
@@ -1,11 +1,12 @@
 import nextra from 'nextra'
  
-// Set up Nextra with its configuration
 const withNextra = nextra({
-  // ... Add Nextra-specific options here
+  latex: true,
+  search: {
+    codeblocks: false
+  },
+  contentDirBasePath: '/docs'
 })
  
-// Export the final Next.js config with Nextra included
 export default withNextra({
-  // ... Add regular Next.js options here
 })
