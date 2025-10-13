8af65961e9a8c89ba0d1484a3dc584cd97cc36c7
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2025-10-13 21:18:26 +1300

    Add docker compose min

---
+++ scripts/start-dependencies-min.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+command=${@:-up -d --build}
+
+cd "$(dirname "${0}")/.."
+
+docker compose -p "skedular" \
+    -f docker-compose-min.yml \
+    --env-file .env \
+    $command
