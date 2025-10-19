fa2bba50d6fbe60bc9bd2f29e9adb84b91cd01c4
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2025-10-19 15:06:59 +1300

    Fixed authkit middleware setup

--- web/apps/webapp/src/middleware.ts
+++ web/apps/webapp/src/middleware.ts
@@ -1,18 +1,12 @@
 import { authkitMiddleware } from '@workos-inc/authkit-nextjs';
 
-export default authkitMiddleware({ debug: true, redirectUri: process.env.NEXT_PUBLIC_WORKOS_REDIRECT_URI! });
+export default authkitMiddleware({
+  middlewareAuth: {
+    enabled: true,
+    unauthenticatedPaths: ['/'],
+  },
+});
 
 export const config = {
-  matcher: [
-    '/:slug*',
-    '/welcome',
-    '/bookings',
-    '/notifications',
-    '/billing-and-payment',
-    '/settings',
-    '/organizations',
-    '/organizations/:slug*',
-    '/notifications',
-    '/notifications/:slug*',
-  ],
+  matcher: ['/welcome', '/bookings', '/notifications', '/billing-and-payment', '/settings', '/organizations', '/organizations/:path*', '/notifications', '/notifications/:path*'],
 };
