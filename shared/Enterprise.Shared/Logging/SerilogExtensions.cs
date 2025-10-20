6a837c392c23a3005b9b548ad2c06523aff8ba1b
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2025-10-21 07:09:21 +1300

    Ensuring that request to health endpoint is not ending in the log

--- shared/Enterprise.Shared/Logging/SerilogExtensions.cs
+++ shared/Enterprise.Shared/Logging/SerilogExtensions.cs
@@ -2,6 +2,7 @@
 using Serilog;
 using Serilog.Enrichers.Sensitive;
 using Serilog.Enrichers.Span;
+using Serilog.Events;
 using Serilog.Formatting.Compact;
 
 namespace Enterprise.Shared.Logging;
@@ -21,6 +22,24 @@ public static class SerilogExtensions
                     .Enrich.WithSpan()
                     .Enrich.FromLogContext()
                     .Enrich.WithSensitiveDataMasking(new SensitiveDataEnricherOptions { Mode = MaskingMode.Globally, MaskValue = "***REDACTED***" })
+                    .Filter.ByExcluding(logEvent =>
+                    {
+                        if (!logEvent.Properties.TryGetValue("SourceContext", out var sourceContextValue) ||
+                            sourceContextValue is not ScalarValue { Value: string sourceContext } ||
+                            sourceContext != "Microsoft.AspNetCore.Hosting.Diagnostics")
+                        {
+                            return false;
+                        }
+
+                        if (!logEvent.Properties.TryGetValue("RequestPath", out var requestPathValue) ||
+                            requestPathValue is not ScalarValue { Value: string requestPath })
+                        {
+                            return false;
+                        }
+
+                        return requestPath.Equals(HealthCheck.Constants.ReadinessPath, StringComparison.InvariantCultureIgnoreCase) ||
+                               requestPath.Equals(HealthCheck.Constants.LivenessPath, StringComparison.InvariantCultureIgnoreCase);
+                    })
                     .ReadFrom.Configuration(hostingContext.Configuration)
                     .WriteTo.Console(new RenderedCompactJsonFormatter());
             },
