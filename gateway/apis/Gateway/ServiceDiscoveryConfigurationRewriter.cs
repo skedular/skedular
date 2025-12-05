9a8354f166ec58c52ea744561791851897f2a1ff
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2025-12-06 11:55:24 +1300

    Updated gateway

--- gateway/apis/Gateway/ServiceDiscoveryConfigurationRewriter.cs
+++ gateway/apis/Gateway/ServiceDiscoveryConfigurationRewriter.cs
@@ -10,57 +10,39 @@ public class ServiceDiscoveryConfigurationRewrite(SubgraphsConfigurations subgra
         {
             nameof(subgraphsConfigurations.Booking) => configuration with
             {
-                EndpointUri = string.IsNullOrWhiteSpace(subgraphsConfigurations.Booking.Uri?.AbsolutePath)
-                    ? new Uri("https+http://bookingapi/v1/graphql")
-                    : subgraphsConfigurations.Booking.Uri
+                EndpointUri = subgraphsConfigurations.Booking.Uri!
             },
             nameof(subgraphsConfigurations.Core) => configuration with
             {
-                EndpointUri = string.IsNullOrWhiteSpace(subgraphsConfigurations.Core.Uri?.AbsolutePath)
-                    ? new Uri("https+http://coreapi/v1/graphql")
-                    : subgraphsConfigurations.Core.Uri
+                EndpointUri = subgraphsConfigurations.Core.Uri!
             },
             nameof(subgraphsConfigurations.Customer) => configuration with
             {
-                EndpointUri = string.IsNullOrWhiteSpace(subgraphsConfigurations.Customer.Uri?.AbsolutePath)
-                    ? new Uri("https+http://customerapi/v1/graphql")
-                    : subgraphsConfigurations.Customer.Uri
+                EndpointUri = subgraphsConfigurations.Customer.Uri!
             },
             nameof(subgraphsConfigurations.Location) => configuration with
             {
-                EndpointUri = string.IsNullOrWhiteSpace(subgraphsConfigurations.MsTeams.Uri?.AbsolutePath)
-                    ? new Uri("https+http://locationapi/v1/graphql")
-                    : subgraphsConfigurations.MsTeams.Uri
+                EndpointUri = subgraphsConfigurations.Location.Uri!
             },
             nameof(subgraphsConfigurations.Marketplace) => configuration with
             {
-                EndpointUri = string.IsNullOrWhiteSpace(subgraphsConfigurations.MsTeams.Uri?.AbsolutePath)
-                    ? new Uri("https+http://marketplaceapi/v1/graphql")
-                    : subgraphsConfigurations.MsTeams.Uri
+                EndpointUri = subgraphsConfigurations.Marketplace.Uri!
             },
             nameof(subgraphsConfigurations.MsTeams) => configuration with
             {
-                EndpointUri = string.IsNullOrWhiteSpace(subgraphsConfigurations.MsTeams.Uri?.AbsolutePath)
-                    ? new Uri("https+http://msteamsapi/v1/graphql")
-                    : subgraphsConfigurations.MsTeams.Uri
+                EndpointUri = subgraphsConfigurations.MsTeams.Uri!
             },
             nameof(subgraphsConfigurations.Organization) => configuration with
             {
-                EndpointUri = string.IsNullOrWhiteSpace(subgraphsConfigurations.Organization.Uri?.AbsolutePath)
-                    ? new Uri("https+http://organizationapi/v1/graphql")
-                    : subgraphsConfigurations.Organization.Uri
+                EndpointUri = subgraphsConfigurations.Organization.Uri!
             },
             nameof(subgraphsConfigurations.Slack) => configuration with
             {
-                EndpointUri = string.IsNullOrWhiteSpace(subgraphsConfigurations.Slack.Uri?.AbsolutePath)
-                    ? new Uri("https+http://slackapi/v1/graphql")
-                    : subgraphsConfigurations.Slack.Uri
+                EndpointUri = subgraphsConfigurations.Slack.Uri!
             },
             nameof(subgraphsConfigurations.Team) => configuration with
             {
-                EndpointUri = string.IsNullOrWhiteSpace(subgraphsConfigurations.Team.Uri?.AbsolutePath)
-                    ? new Uri("https+http://teamapi/v1/graphql")
-                    : subgraphsConfigurations.Team.Uri
+                EndpointUri = subgraphsConfigurations.Team.Uri!
             },
             _ => throw new NotSupportedException()
         });
